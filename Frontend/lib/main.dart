5be9643fdf13e59fe1177bee9508578dc59e4a22
Shani <shanithecoder@gmail.com>
2025-11-27 15:21:37 +0530

    Added sidebar

--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/pages/loginPage.dart';
 import 'package:frontend/pages/tab.dart';
+import 'package:frontend/theme_colors.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sidebarx/sidebarx.dart';
+import 'package:flex_sidebar/flex_sidebar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +13,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: TabHomePage());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
   }
 }
 
@@ -21,24 +24,49 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Acadence")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Login'),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const LoginScreen()),
-                );
-              },
-              child: const Text("Login"),
+      body: Row(
+        children: [
+          FlexSidebar(
+            controller: FlexSidebarController(resizeAnimCurve: Curves.linear),
+            theme: FlexThemeData(
+              normalWidth: 200,
+              normalDecoration: BoxDecoration(
+                color: ApplicationColors.primaryBlue,
+                borderRadius: BorderRadius.horizontal(),
+              ),
+              minimizedDecoration: BoxDecoration(
+                color: ApplicationColors.primaryBlue,
+                borderRadius: BorderRadius.horizontal(),
+              ),
+              scrollableItems: true,
+              itemsAlignment: MainAxisAlignment.start,
             ),
-          ],
-        ),
+            primaryWidget: Icon(Icons.account_circle),
+            secondaryWidget: Text(
+              'User001',
+              style: GoogleFonts.poppins(
+                textStyle: TextStyle(fontWeight: FontWeight.w500),
+              ),
+            ),
+            items: [
+              FlexSidebarItem(
+                icon: Icon(Icons.accessibility_new_outlined),
+                label: Text('Accessibility'),
+                onTap: () {
+                  print('Accessibility tapped');
+                },
+              ),
+              FlexSidebarItem(
+                icon: Icon(Icons.settings),
+                label: Text('Settings'),
+                onTap: () {
+                  print('Settings tapped');
+                },
+              ),
+            ],
+          ),
+          Expanded(child: TabHomePage()),
+        ],
       ),
     );
   }
